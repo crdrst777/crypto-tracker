a79d2bf3cec13995f293a5a06fb14e59d9352677
HYEJINLEE <crdrst777@gmail.com>
2023-02-22 11:01:41 +0900

    setting

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coin = () => {
+  return <h1>Coin</h1>;
+};
+
+export default Coin;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Coin = () => {
+  return <h1>Coins</h1>;
+};
+
+export default Coin;
