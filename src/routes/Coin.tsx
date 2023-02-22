19b788c91c5b6916b213b4ac31dabc2db111170b
HYEJINLEE <crdrst777@gmail.com>
2023-02-22 17:29:34 +0900

    feat: route states 작업

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,63 @@
-import { useParams } from "react-router-dom";
+// 아래 에러는 <Title>{state?.name || "Loading.."}</Title> 이걸로 잡아줬음.
+// http://localhost:3000/btc-bitcoin 예를들어 이렇게 coinId가 들어간 url로 바로 들어가려면 에러가 남.
+// state가 생성되려면 home(Coins)화면을 먼저 열어야하기 때문임. 그다음 코인목록에서 코인을 클릭해야 Coin 화면으로 넘어감.
+// Coins.tsx에서 <Link to={`/${coin.id}`} state={{ name: coin.name }}> 로 name을 넘겨주고,
+// Coin.tsx 여기서 const { state } = useLocation() as RouteState; 으로 받아왔기 때문임.
+
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  /* font-size: 48px; */
+  font-size: 18px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteState {
+  state: {
+    name: string;
+  };
+}
 
 const Coin = () => {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   // url의 파라미터 부분을 잡아내고 싶을때 useParams훅을 쓴다.
   // const params = useParams(); 이렇게 쓰면 {coinId: 'ㅌㅌㅌ'} 로 나옴. 여기서 coinId를 꺼내온거임.
   // useParams 쓰는 순간 타입이 자동으로 string or undefined로 지정됨.
 
-  console.log(coinId);
+  const { state } = useLocation() as RouteState;
+  // 원래 location.state.name 이런 복잡한 구조로 되있는데 RouteState 타입을 지정해줬기때문에, 위의 {state}처럼 간단히 정의할수가 있음.
 
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+        {/* state가 존재하면 name을 가져오고, 존재하지 않으면 "Loading.."을 보여줘라.. */}
+      </Header>
+
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 };
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+// import axios from "axios";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,13 +17,20 @@ const Header = styled.header`
 `;
 
 const Title = styled.h1`
-  font-size: 48px;
+  /* font-size: 48px; */
+  font-size: 18px;
   color: ${(props) => props.theme.accentColor};
 `;
 
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 const CoinsList = styled.ul``;
@@ -34,9 +41,11 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px; // padding을 위에서가 아닌 여기서 주니깐 카드 면적이 전부 클릭 가능한 영역이 됨
     transition: color 0.2s ease-in;
-    display: block; // 이러면 카드의 끝부분까지도 클릭 가능
+    /* display: block; // 이러면 카드의 끝부분까지도 클릭 가능 */
   }
   &:hover {
     a {
@@ -60,15 +69,15 @@ const Coins = () => {
   const [loading, setLoading] = useState(true);
 
   // axios와 try, catch를 사용한 방식
-  const getCoins = async () => {
-    try {
-      const response = await axios.get("https://api.coinpaprika.com/v1/coins");
-      setCoins(response.data.slice(0, 100)); // 앞에서부터 100개만 보여주기
-      setLoading(false);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+  // const getCoins = async () => {
+  //   try {
+  //     const response = await axios.get("https://api.coinpaprika.com/v1/coins");
+  //     setCoins(response.data.slice(0, 100)); // 앞에서부터 100개만 보여주기
+  //     setLoading(false);
+  //   } catch (error) {
+  //     console.log("error", error);
+  //   }
+  // };
 
   // axios를 사용한 방식
   // const getCoins = async () => {
@@ -79,14 +88,14 @@ const Coins = () => {
   // };
 
   // 강의에 나온 방식 (조금 다름)
-  // const getCoins = async () => {
-  //   const json = await (
-  //     await fetch("https://api.coinpaprika.com/v1/coins")
-  //   ).json();
-  //   // console.log(json);
-  //   setCoins(json.slice(0, 100)); // 앞에서부터 100개만 보여주기
-  //   setLoading(false);
-  // };
+  const getCoins = async () => {
+    const json = await (
+      await fetch("https://api.coinpaprika.com/v1/coins")
+    ).json();
+    // console.log(json);
+    setCoins(json.slice(0, 100)); // 앞에서부터 100개만 보여주기
+    setLoading(false);
+  };
 
   console.log(coins);
 
@@ -106,7 +115,14 @@ const Coins = () => {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              {/* <Link to={`/${coin.id}`}> */}
+              <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                <Img
+                  // src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
               {/* {/:coinId} */}
             </Coin>
           ))}
